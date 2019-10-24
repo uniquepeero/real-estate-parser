65a4d5c55300ee9a3adb7ea1b6f473530034611f
Vyacheslav <48791104+uniquepeero@users.noreply.github.com>
2019-10-24 19:19:11 +0500

    Added features, removed garbage strings

--- main.py
+++ main.py
@@ -1,84 +1,100 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+from dotenv import load_dotenv
 import logging
 import os
 import requests
-import configparser
+import re
+
 
 path_to_driver = f'{os.getcwd()}\\geckodriver.exe'
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
-fh = logging.FileHandler("parser_log.log", 'w', encoding="utf-8")
+fh = logging.FileHandler("parser.log", 'w', encoding="utf-8")
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 log.addHandler(fh)
 
 driver = webdriver.Firefox(executable_path=path_to_driver)
 
-def parseavito():
-	try:
-		url = "AVITOURL"
-		driver.implicitly_wait(30)
-		log.debug('Loading avito page...')
-		driver.get(url)
-		log.debug('Loaded.')
-		sleep(3)
-	except Exception as e:
-		log.error(e)
+wait = WebDriverWait(driver, 60)
+
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(env_path):
+	load_dotenv(env_path)
+else:
+	log.error('.env file not found')
+
+API_KEY = os.getenv('API_KEY')
+CHAT_ID = os.getenv('CHAT_ID')
+
+
+def parse_avito():
+	url = "AVITOLINK"
+	driver.implicitly_wait(30)
+	log.debug('Loading avito page...')
+	driver.get(url)
+	log.debug('Loaded.')
 
 	titles_dict = {}
-	while True:
-		try:
-			titles = driver.find_elements_by_class_name('item-description-title-link')
-			break
-		except:
-			pass
+	titles = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'item-description-title-link')))
+
 	for title in titles:
 		titles_dict[title.text] = title.get_attribute('href')
 
 	while True:
-		sleep(5)
-		newtitles_dict = {}
-		driver.refresh()
-		while True:
-			try:
-				newtitles = driver.find_elements_by_class_name('item-description-title-link')
-				break
-			except:
-				pass
-		for newtitle in newtitles:
-			newtitles_dict[newtitle.text] = newtitle.get_attribute('href')
-
 		msg = """"""
-		for name, href in newtitles_dict.items():
+		sleep(5)
+
+		new_titles_dict = {}
+		driver.refresh()
+		new_titles = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'item-description-title-link')))
+
+		for new_title in new_titles:
+			new_titles_dict[new_title.text] = new_title.get_attribute('href')
+
+		for name, href in new_titles_dict.items():
 			if name not in titles_dict.keys():
 				log.debug(f'Found new: {name}')
 				log.debug(f'href: {href}')
-				msg += f'{name}\n{href}\n\n'
-		if len(msg) > 1:
-			send(msg)
 
-		titles_dict = newtitles_dict
+				driver.execute_script(f'window.open("{href}","_blank");')
+				driver.switch_to.window(driver.window_handles[1])
+
+				views = driver.find_element_by_xpath("//div[@class='title-info-metadata-item title-info-metadata-views']")
+				views = re.search(r'(\d+)', views.text)
+				views = int(views.group(0))
+
+				if views < 200:
+					msg += f'{name}\n{href}\n\n'
+				else:
+					log.debug(f'Too many views: {views}')
+
+				driver.execute_script('window.close();')
+				driver.switch_to.window(driver.window_handles[0])
+
+		if msg:
+			send(msg)
+		titles_dict = new_titles_dict
+
 
 def send(message):
-	proxies = {
-		'http': '',
-		'https': ''
-	}
-	url = f'https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text={message}'
+	url = f'https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id={CHAT_ID}&text={message}'
 	try:
-		res = requests.get(url, proxies=proxies)
+		res = requests.get(url)
 		if res.status_code == requests.codes.ok:
-			log.debug(f'Message sended {message}')
+			log.debug(f'Successful send: {message}')
 		else:
-			log.error(f'Failed TG send: {res.status_code}')
+			log.error(f'Send failed: {res.status_code}')
 	except requests.exceptions.RequestException:
-		log.error('Failed TG send', exc_info=True)
+		log.error('Send failed', exc_info=True)
 	except ValueError:
-		log.error(f'Failed TG sent: {res.text}')
+		log.error(f'Send failed: {res.text}')
 
 
 if __name__ == '__main__':
-    parseavito()
-
+	parse_avito()
